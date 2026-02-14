4e9485caca197801657384fd2aa9c0861eacb73a
hithesh <hithesh>
2026-02-14 21:25:56 +0530

    feat: Add Mixpanel dependency and initialize SDK for analytics.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,12 @@
+//Import Mixpanel SDK
+import mixpanel from "mixpanel-browser";
+
+// Create an instance of the Mixpanel object, your token is already added to this snippet
+mixpanel.init('29b61feb2990595f565b2ff83078b5f3', {
+  autocapture: true,
+  record_sessions_percent: 100,
+})
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
